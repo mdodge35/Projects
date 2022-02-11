38c0a9420d7c0bb584dde8583269c5b08da897a7
mdodge35 <99460393+mdodge35@users.noreply.github.com>
2022-02-10 19:56:43 -0800

    Add files via upload

---
+++ Number Guessing Game(User Guesses)/main.py
@@ -0,0 +1,15 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1,x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print("Too low, guess again!")
+        elif guess > random_number:
+            print("Too high, guess again!")
+
+    print(f"Congrats! you guessed the number {random_number} correctly")
+
+guess(100)
